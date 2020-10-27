dcde3154a64e5e9f57082ae487840918ab61e4a4
Lucas Soares <lucascsoares21@gmail.com>
2020-10-27 16:00:39 -0300

    Create 1016.cpp

---
+++ Begginner/1016.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solving(){
+    int y;
+    cin>>y;
+
+    //y + 30km;x
+    //a cada 2 minutos o carro y se distancia 1km
+
+    cout << y*2 << " minutos" << endl;
+}
+int main()
+{
+    solving();
+}

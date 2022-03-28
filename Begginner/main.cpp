6f04b625af44f035e1dba9065846204328b4df8f
Lucas Soares <68960576+SagHuns@users.noreply.github.com>
2022-03-28 20:28:46 -0300

    Add files via upload

---
+++ Begginner/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0){
+        cout << "Valores aceitos";
+    }
+    else{
+        cout << "Valores nao aceitos";
+    }
+return 0;
+}
